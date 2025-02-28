e5a25144802574ff36934cd6a70c1da630aae168
disco-quotidie <disco.quotidie@outlook.com>
2025-02-28 06:54:55 -0800

    feat: update coinbase tx parse logic

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
 const { Transaction } = require('bitcoinjs-lib')
 const COINBASE_INPUT_HASH = "0000000000000000000000000000000000000000000000000000000000000000"
+const TRANSACTION_TYPE = {
+  NORMAL: "normal",
+  COINBASE: "coinbase",
+  UNKNOWN: "unknown"
+}
 
 const parseTxHex = (txHex) => {
   const tx = Transaction.fromHex(txHex)
   const isCoinbase = isCoinbaseTx(tx)
-
-  if (tx && tx.ins && tx.ins.length > 0) {
-    for (const _in_ of tx.ins) {
-      const scriptBuffer = Buffer.from(_in_.script)
-      console.log(scriptBuffer.toString('hex'))
-      const firstByte = scriptBuffer[0];
-
-      // convert firstByte to integer
-      const firstByteInt = parseInt(firstByte)
-
-      // const headerBytes = scriptBuffer.slice(0, firstByteInt + 1).toString('hex');
-      let messageBytes = scriptBuffer.slice(firstByteInt + 1).toString('hex');
-      if (messageBytes.startsWith("010445")) {
-        messageBytes = messageBytes.slice(6)
-      }
-      const message = hexToAscii(messageBytes)
-      return {
-        txType: "coinbase",
-        message
-      }
-    }
-  }
+  if (isCoinbase)
+    return parseCoinbaseTransaction(tx)
+  else
+    console.log(`Anyway, not a coinbase`)
 }
 
 const isCoinbaseTx = (tx) => {
@@ -33,6 +20,33 @@ const isCoinbaseTx = (tx) => {
   if (ins && ins.length > 0) {
     const { hash } = ins[0]
     return hash.toString('hex') === COINBASE_INPUT_HASH
+  }
+}
+
+const parseCoinbaseTransaction = (tx) => {
+  let msgHex = "";
+  let message = "";
+
+  if (tx && tx.ins && tx.ins.length > 0) {
+    for (const _in_ of tx.ins) {
+      const scriptBuffer = Buffer.from(_in_.script)
+      msgHex = scriptBuffer.toString('hex')
+      message = hexToAscii(scriptBuffer.toString('hex'))
+
+      // edge cases
+      const firstByte = scriptBuffer[0];
+      const firstByteInt = parseInt(firstByte)
+      let messageBytes = scriptBuffer.slice(firstByteInt + 1).toString('hex');
+      if (messageBytes.startsWith("010445")) {
+        messageBytes = messageBytes.slice(6)
+        message = hexToAscii(messageBytes)
+      }
+    }
+  }
+  return {
+    txType: TRANSACTION_TYPE.COINBASE,
+    msgHex,
+    message
   }
 }
 
